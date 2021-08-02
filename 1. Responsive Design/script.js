4f0e43030a5a9c0aed48e9491927170ddb083802
abdullahbasal <56451638+abdullahbasal@users.noreply.github.com>
2021-08-02 14:54:09 +0300

    Add files via upload

---
+++ 1. Responsive Design/script.js
@@ -0,0 +1,30 @@
+// var yas = 31;
+// if (yas > 30) {
+//   alert("yas 30dan buyuk");
+// } else {
+//   alert("yas 30dan kucuk veya esit");
+// }
+
+// point >= 80 && point <= 100;
+// var point = parseInt(Math.random(0, 100) * 100, 10);
+// alert(point);
+// if (point > 0 && point <= 40) {
+//   alert("FF");
+// } else if (point > 40 && point <= 60) {
+//   alert("DD");
+// } else if (point > 60 && point <= 70) {
+//   alert("CC");
+// } else if (point > 70 && point <= 80) {
+//   alert("BB");
+// } else if (point > 80 && point <= 100) {
+//   alert("AA");
+// } else {
+//   alert("Aralik Disi");
+// }
+// var comingBus = "a";
+
+// if (comingBus == "9UD" || comingBus == "11US") {
+//   alert("gidebilir");
+// } else {
+//   alert("gidemez");
+// /
